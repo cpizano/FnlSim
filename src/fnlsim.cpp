2e8e991487ce2fd15c63b63697e0d8d992e53476
Carlos Pizano <cpu@chromium.org>
2016-01-17 11:33:14 -0800

    add a simulation layer
    feeling hardcore so going with 4 threads to simulate 4 cores so that means we need to have a more realistic scheduler which might be tricky in x64 user mode msvc.

--- src/fnlsim.cpp
+++ src/fnlsim.cpp
@@ -95,7 +95,7 @@ void init_thread_obj(FnlThread* thread, FnlProcess* proc, uint64_t id) {
 
 void make_kernel_process() {
   FnlProcess* proc = kheap::make_isll_node<FnlProcess>();
-  init_process_obj(proc, 1, ":kernel");
+  init_process_obj(proc, 1, ":sys0");
 
   FnlThread* thread = kheap::make_isll_node<FnlThread>();
   init_thread_obj(thread, proc, 1);
@@ -114,10 +114,79 @@ void init() {
 
 }
 
-int main() {
+
+namespace sim {
+
+struct Core {
+  HANDLE win_th;
+  CONTEXT ctx;
+};
+
+const int num_cores = 4;
+Core cores[num_cores];
+HANDLE cport;
+
+unsigned long __stdcall holder_tfn(void*) {
+  KPANIC;
+  return 0;
+}
+
+void init_core(Core* core) {
+  core->win_th = ::CreateThread(nullptr, 0, holder_tfn, nullptr, CREATE_SUSPENDED, nullptr);
+  CHECKNE(core->win_th, nullptr);
+  core->ctx.ContextFlags = CONTEXT_ALL;
+  ::GetThreadContext(core->win_th, &core->ctx);
+}
+
+void core_start(int core_id, void* start) {
+  Core& core = cores[core_id];
+  // This only works with newly created (suspended) threads.
+  core.ctx.Rcx = (DWORD64)start;
+  core.ctx.ContextFlags = CONTEXT_INTEGER;
+  ::SetThreadContext(core.win_th, &core.ctx);
+  ::ResumeThread(core.win_th);
+}
+
+void init() {
+  cport = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 1);
+  CHECKNE(cport, nullptr);
+  for (int ix = 0; ix != num_cores; ++ix) {
+    init_core(&cores[ix]);
+  }
+}
+
+void run() {
+  unsigned long bytes = 0;
+  UINT_PTR key = 0;
+  OVERLAPPED* ov = nullptr;
+
+  wprintf(L"fnl sim running. %d cores x64\n", num_cores);
+
+  while (true) {
+    BOOL rv =::GetQueuedCompletionStatus(cport, &bytes, &key, &ov, INFINITE);
+    if (!rv) {
+      wprintf(L"got cp error\n");
+    }
+    else {
+      wprintf(L"got event. exiting\n");
+      break;
+    }
+  }
+}
+
+}
+
+unsigned long __stdcall fnl_init(void*) {
   kheap::init();
   vmm::init();
   exec::init();
+  return 0;
+}
+
+int main() {
+  sim::init();
+  sim::core_start(0, &fnl_init);
+  sim::run();
   return 0;
 }
 
